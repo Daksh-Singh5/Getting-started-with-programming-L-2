2ec56e318f6e2834aad222cc40dc3bae5a66e09c
Daksh-Singh5 <dakshsingh6612@gmail.com>
2024-11-24 19:48:59 +0530

    first commit

---
+++ index.py
@@ -0,0 +1,9 @@
+name = "Daksh Singh"
+Class = 7
+school = "Thakur Ramnarayan Public School"
+
+print("my Name is",name )
+print("my class is",Class )
+print("my School is",school )
+
+import k
---
+++ index1.py
@@ -0,0 +1,6 @@
+import keyword
+
+
+print("the list of keyword of python are")
+print(keyword.kwlist)
+print(len(keyword.kwlist))
